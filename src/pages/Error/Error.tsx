e41c6a9be97b2f2ef0ac30d478f7abd63a2c1ab8
Arseny <phone.kulikovarseniy@gmail.com>
2024-06-13 16:49:16 +0300

    error page add

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,3 @@
+export default function ErrPage() {
+  return <h1> 404: Страница не найдена</h1>;
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,14 @@
-import { Route, Routes} from 'react-router-dom';
-import Login from '../pages/Login/Login';
-import UserPage from '../pages/User/UserPage';
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import UserPage from "../pages/User/UserPage";
+import ErrPage from "../pages/Error/Error";
 
 export default function AppRoutes() {
-  
   return (
     <Routes>
-      <Route path="/" element={<Login/>}/> 
-      <Route path="/user" element={<UserPage/>}/>      
+      <Route path="/" element={<Login />} />
+      <Route path="/user" element={<UserPage />} />
+      <Route path="*" element={<ErrPage />} />
     </Routes>
   );
 }
